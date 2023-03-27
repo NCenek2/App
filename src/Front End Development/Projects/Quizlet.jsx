ef4c0403e3c7360867b312f8ee475ea6731e7f9f
NCenek2 <ceneknicholas@gmail.com>
2023-03-27 17:41:31 -0400

    quizlet layout/ regex to seperate by spaces

--- src/Front End Development/Projects/Quizlet.jsx
+++ src/Front End Development/Projects/Quizlet.jsx
@@ -34,6 +34,66 @@ const Quizlet = () => {
     write: false,
   });
 
+  React.useEffect(() => {
+    const handleEnter = (event) => {
+      if (event.key == "Enter" && checkValue == null) {
+        event.preventDefault();
+        handleCheck();
+      }
+      return;
+    };
+
+    if (mode.write && checkValue == null) {
+      window.addEventListener("keypress", handleEnter);
+    }
+    return () => window.removeEventListener("keypress", handleEnter);
+  }, [inputValue, checkValue]);
+
+  React.useEffect(() => {
+    const handleNext = (event) => {
+      if (event.key == "Enter" && checkValue == true) {
+        event.preventDefault();
+        handleNextTerm();
+      }
+      return;
+    };
+
+    if (mode.write && isOverwritten) {
+      window.addEventListener("keypress", handleNext);
+    }
+    return () => window.removeEventListener("keypress", handleNext);
+  }, [inputValue, checkValue, isOverwritten]);
+
+  React.useEffect(() => {
+    const handleFalseKeyOverride = (event) => {
+      if (event.key == "O" && checkValue == false) {
+        event.preventDefault();
+        handleOverwrite();
+      }
+      return;
+    };
+    if (mode.write) {
+      window.addEventListener("keypress", handleFalseKeyOverride);
+    }
+    return () => window.removeEventListener("keypress", handleFalseKeyOverride);
+  }, [inputValue, checkValue]);
+
+  React.useEffect(() => {
+    const handleFalseKeyNext = (event) => {
+      console.log(event.key);
+      if (event.key == "N" && checkValue == false) {
+        event.preventDefault();
+        handleNextTerm();
+      }
+      return;
+    };
+
+    if (mode.write) {
+      window.addEventListener("keypress", handleFalseKeyNext);
+    }
+    return () => window.removeEventListener("keypress", handleFalseKeyNext);
+  }, [inputValue, checkValue]);
+  // End Keyboard Event
   const handleData = () => {
     // Checks if cards can be made. Anything check, # and : check
     if (!droppedData || !/.*[a-z]{2,}.*:.*[a-z]{2,}.*#/gi.test(droppedData)) {
@@ -147,19 +207,16 @@ const Quizlet = () => {
     for (let item of matched) {
       if (!unWanted.has(item) && item.length > 1) mySet.add(item);
     }
+    matched = Array.from(mySet);
     return [mySet, matched];
   };
   const handleCheck = () => {
     if (inputValue.length <= 1) return;
-
     const [definitionSet, undefined] = createSet(data[index].definition);
     let [userSet, userArray] = createSet(inputValue);
     if ((!userSet && !userArray) || userSet.size < 1) return;
-    console.log("---------");
-    console.log(definitionSet, "def set");
-    console.log(userSet, "user set");
-    let count = 0;
     let usedSet = new Set();
+    let count = 0;
     // Currently overriding wrong data. no sum
     setWrongData((p) => {
       let output = [...p];
@@ -168,30 +225,43 @@ const Quizlet = () => {
         return output;
       }
       for (let i = 0; i < userArray.length; i++) {
-        if (definitionSet.has(userArray[i]) && !usedSet.has(userArray[i])) {
-          usedSet.add(userArray[i]);
+        if (definitionSet.has(userArray[i])) {
+          console.log("HAS", userArray[i], count + 1);
           count++;
         }
-        // Check verification status 30% or defintion size
-        if (
-          count > Math.ceil(definitionSet.size * 0.3) ||
-          count == definitionSet.size
-        ) {
-          setCheckValue((p) => true);
-          return output;
+        // Check verification status 60% or defintion size
+      }
+      if (count == definitionSet.size) {
+        console.log("All Right!", count, userSet.size, definitionSet.size);
+        setCheckValue((p) => true);
+        setPlaceholder(
+          (p) =>
+            `Perfect!\nYours:\n${inputValue}\nActual:\n${data[index].definition}`
+        );
+        setInputValue((p) => "");
+        return output;
+      } else if (count >= Math.ceil(definitionSet.size * 0.6)) {
+        setCheckValue((p) => true);
+        setPlaceholder(
+          (p) =>
+            `Correct!\nYours:\n${inputValue}\nActual:\n${data[index].definition}`
+        );
+        setInputValue((p) => "");
+        return output;
+      } else {
+        if (!wrongDataSet.has(data[index].term)) {
+          // Add term to wrong set after failing check
+          setWrongDataSet((p) => p.add(data[index].term));
+          output.push(data[index]);
         }
+        setCheckValue((p) => false);
+        setPlaceholder(
+          (p) => `Yours:\n${inputValue}\nActual:\n${data[index].definition}`
+        );
+        setInputValue((p) => "");
+        return output;
       }
-      // Add term to wrong set after failing check
-      if (!wrongDataSet.has(data[index].term)) {
-        setWrongDataSet((p) => p.add(data[index].term));
-        output.push(data[index]);
-      }
-      setCheckValue((p) => false);
-      setPlaceholder(
-        (p) => `Yours:\n${inputValue}\nActual:\n${data[index].definition}`
-      );
-      setInputValue((p) => "");
-      return output;
+      // Handle display terms
     });
   };
 
@@ -246,38 +316,46 @@ const Quizlet = () => {
   if (!dataProcess) {
     return (
       <React.Fragment>
-        <div className="quizlet-body quizlet-front">
-          <p className="quizlet-directions">
-            Term and definition pairs should be written in the following
-            format...
-          </p>
-          <p className="quizlet-directions">TERM: DEFINITION#</p>
-          <p className="quizlet-directions">
-            Make pairs as concise as possible. Terms and Definitions contain at
-            least 2 or more consecutive letters. Do not use ":" except to
-            seperate term and defintion For example,...
-          </p>
-          <p className="quizlet-directions">
-            Median Value: Value at 1/2 of data length.#
-          </p>
-          <p className="quizlet-directions">
-            For the write session, one letter words, patterns that do not have
-            two consectuive letters, and common articles, prepositions, etc are
-            not included.
-          </p>
-          <p className="quizlet-directions">
-            Not Included: "the","an","at","so","is","are",
-            "of","and","in","there", "("
-          </p>
-          <textarea
-            name="droppedData"
-            value={droppedData}
-            onChange={(e) => setDroppedData(e.target.value)}
-            className="droppedData"
-          ></textarea>
-          <button className="btn btn-primary" onClick={() => handleData()}>
-            Set Cards
-          </button>
+        <div className="quizlet-front">
+          <div className="quizlet-directions-container">
+            <p className="quizlet-directions">
+              Term and definition pairs should be written in the following
+              format...
+            </p>
+            <p className="quizlet-directions">TERM: DEFINITION#</p>
+            <p className="quizlet-directions">
+              Make pairs as concise as possible. Terms and Definitions contain
+              at least 2 or more consecutive letters. Do not use ":" except to
+              seperate term and defintion For example,...
+            </p>
+            <p className="quizlet-directions">
+              Median Value: Value at 1/2 of data length.#
+            </p>
+            <p className="quizlet-directions">
+              For the write session, one letter words, patterns that do not have
+              two consectuive letters, and common articles, prepositions, etc
+              are not included.
+            </p>
+            <p className="quizlet-directions">
+              Not Included: "the","an","at","so","is","are",
+              "of","and","in","there", "("
+            </p>
+          </div>
+
+          <div className="quizlet-data-container">
+            <textarea
+              name="droppedData"
+              value={droppedData}
+              onChange={(e) => setDroppedData(e.target.value)}
+              className="droppedData"
+            ></textarea>
+            <button
+              className="set-cards-btn btn btn-primary"
+              onClick={() => handleData()}
+            >
+              Set Cards
+            </button>
+          </div>
         </div>
       </React.Fragment>
     );
@@ -286,7 +364,7 @@ const Quizlet = () => {
   if (finishedCards) {
     return (
       <React.Fragment>
-        <div className="quizlet-body">
+        <div className="quizlet-finished-container">
           <h1 className="quizlet-finished">You're Done!</h1>
           <button className="btn btn-light" onClick={() => handleFinish()}>
             Go Back!
@@ -298,10 +376,10 @@ const Quizlet = () => {
 
   return (
     <React.Fragment>
-      <div className="quizlet-body">
-        <div className="quizlet-modes">
+      <div className="quizlet-container">
+        <div className="quizlet-modes-container">
           <button
-            className={`btn btn${
+            className={`quizlet-btn btn btn${
               mode.flashcards ? "-light" : "-outline-light"
             }`}
             onClick={() => handleMode("flashcards")}
@@ -309,88 +387,91 @@ const Quizlet = () => {
             Flashcards
           </button>
           <button
-            className={`btn btn${mode.write ? "-light" : "-outline-light"}`}
+            className={`quizlet-btn btn btn${
+              mode.write ? "-light" : "-outline-light"
+            }`}
             onClick={() => handleMode("write")}
           >
             Write
           </button>
         </div>
         {mode.flashcards && (
-          <React.Fragment>
-            <div
-              className={`quizlet-words ${showDefinition && "term definition"}`}
-              onClick={() => handleShow()}
-            >
-              <p
-                className={`card-identifier ${showDefinition && "definition"}`}
-              >
+          <div className="flashcards-write-container">
+            <div className="flashcards-info">
+              <p className="term-definition">
                 {showDefinition ? "Definition" : "Term"}
               </p>
               <p
-                className={`quizlet-index ${
+                className={`flashcards-index ${
                   index == data.length - 1 && "last-index"
-                } ${showDefinition && "definition"}`}
+                }`}
               >
                 {index + 1}/{data.length}
               </p>
-              {!showDefinition && <p>{data[index].term}</p>}
-              {showDefinition && (
-                <p className="definition">{data[index].definition}</p>
-              )}
             </div>
-            <div className="quizlet-btn-group">
+            <div className="flashcard" onClick={() => handleShow()}>
+              <p className="flashcards-term-definition">
+                {showDefinition ? data[index].definition : data[index].term}
+              </p>
+            </div>
+            <div className="flashcards-btn-group">
               <button
-                className="btn btn-primary"
+                className="btn btn-primary quizlet-btn"
                 onClick={() => handlePrevTerm()}
               >
                 {"<-"}
               </button>
               <button
-                className="btn btn-outline-light"
+                className="btn btn-outline-light quizlet-btn"
                 onClick={() => randomize()}
               >
                 Randomize
               </button>
               <button
-                className="btn btn-primary"
+                className="btn btn-primary quizlet-btn"
                 onClick={() => handleNextTerm()}
               >
                 {"->"}
               </button>
             </div>
-          </React.Fragment>
+          </div>
         )}
         {mode.write && (
-          <div className="quizlet-write-container">
-            <div className="write-term-btn">
-              <span className="write-term">
-                {data[index].term} {""}
-                <span className={`${index == data.length - 1 && "last-index"}`}>
-                  {index + 1}/{data.length}
+          <div className="flashcards-write-container">
+            <div className="write-header">
+              <div className="write-term-container">
+                <span className="write-term">
+                  {data[index].term.substring(0, 49).toUpperCase().slice(0, 1) +
+                    data[index].term.substring(0, 49).slice(1, 50)}{" "}
+                  {""}
+                  <span
+                    className={`${index == data.length - 1 && "last-index"}`}
+                  >
+                    {index + 1}/{data.length}
+                  </span>
                 </span>
-              </span>
-              {checkValue !== null && (
-                <button
-                  className="btn btn-primary btn-sm"
-                  onClick={() => handleNextTerm()}
-                >
-                  {"->"}
-                </button>
-              )}
+              </div>
+
+              <button
+                className="btn btn-primary write-next-btn quizlet-btn"
+                onClick={() => handleNextTerm()}
+                disabled={checkValue == null && "true"}
+              >
+                {"->"}
+              </button>
             </div>
             <textarea
               name="quizletInput"
               value={inputValue}
               onChange={(event) => handleInputValue(event)}
-              className="quizletInput"
+              className="flashcard"
               placeholder={
-                checkValue == false || isOverwritten
-                  ? `${placeholder}`
-                  : "Type the Definition"
+                checkValue != null ? `${placeholder}` : "Type the Definition"
               }
+              disabled={checkValue != null && "true"}
             ></textarea>
             <button
-              className={`check-btn btn btn-${
+              className={`quizlet-btn check-btn btn btn-${
                 checkValue == true
                   ? "success"
                   : checkValue == false
@@ -398,17 +479,17 @@ const Quizlet = () => {
                   : "outline-light"
               }`}
               onClick={() => handleCheck()}
+              disabled={checkValue != null && "true"}
             >
               Check
             </button>
-            {checkValue == false && (
-              <button
-                className="btn btn-outline-light"
-                onClick={() => handleOverwrite()}
-              >
-                Overwrite
-              </button>
-            )}
+            <button
+              className="btn btn-outline-light quizlet-btn"
+              onClick={() => handleOverwrite()}
+              disabled={checkValue == null && "true"}
+            >
+              Overwrite
+            </button>
           </div>
         )}
       </div>
